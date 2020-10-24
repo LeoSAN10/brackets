20485f72fbcf2e3437df2c2830adda3965b2750a
“daniil <jonzi00@mail.ru>
2020-10-25 01:03:40 +0300

    refactor: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var arr = [],
+        new_str = str;
+
+    for (const elem of bracketsConfig) {
+        arr.push(elem[0] + elem[1]);
+    }
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            new_str = new_str.replace(arr[j], '');
+        }
+        if (new_str.length == 0) {
+            return true;
+        }
+    }
+    return false;
 }
